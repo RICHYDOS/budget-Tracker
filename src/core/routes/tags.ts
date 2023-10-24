b0824a07e7239441a5e08b553588c5c8f28a8c5b
daudujohn <daudujoe@gmail.com>
2023-10-24 17:20:35 +0100

    feat: add tag routes

---
+++ src/core/routes/tags.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+
+const router: Router = Router()
+
+router.get('/', () => {})
+
+router.post('/', () => {})
+
+router.put('/:tagId', () => {})
+
+router.delete('/:tagId', () => {})
+
+export { router as tagRoutes }
